6d18a6746c757c21db7753fc269c3dcbf76ae897
Raul Somesan <raulfabiansome@gmail.com>
2026-07-03 16:05:16 +0300

    08.02 Table view not done! 5&6

--- src/main/java/mpp/telodon/controller/MainController.java
+++ src/main/java/mpp/telodon/controller/MainController.java
@@ -1,13 +1,43 @@
 package mpp.telodon.controller;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import mpp.telodon.model.Caritabil;
 import mpp.telodon.service.CaritabilService;
 import mpp.telodon.service.DonatieService;
 import mpp.telodon.service.DonatorService;
 
+
 public class MainController {
+    @FXML
+    private TableView<Caritabil> caritabilTable;
+    @FXML
+    private TableColumn<Caritabil, String> denumireColumn;
+    @FXML
+    private TableColumn<Caritabil, Number> totalColumn;
+
     private CaritabilService caritabil_service;
     private DonatorService donator_service;
     private DonatieService donatie_service;
+
+    public MainController() {
+        denumireColumn.setCellValueFactory(data ->
+                new SimpleStringProperty(data.getValue().getDenumire()));
+        totalColumn.setCellValueFactory(data ->
+                new SimpleDoubleProperty(data.getValue().getTotal()));
+        refreshCaritabilTable();
+    }
+
+    private void refreshCaritabilTable() {
+        ObservableList<Caritabil> items = FXCollections.observableArrayList();
+        caritabil_service.getAll().forEach(items::add);
+        caritabilTable.setItems(items);
+    }
 
     public void setCaritabil_service(CaritabilService caritabil_service) {
         this.caritabil_service = caritabil_service;
@@ -26,4 +56,6 @@ public class MainController {
         setDonatie_service(donatieService);
         setDonator_service(donatorService);
     }
+
+
 }
